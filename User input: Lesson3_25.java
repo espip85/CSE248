93b0b5f61a1099659c8997afdc5191a9be0a6144
espip85 <32580088+espip85@users.noreply.github.com>
2018-10-17 18:52:15 -0400

    Create User input: Lesson3_25.java

---
+++ User input: Lesson3_25.java
@@ -0,0 +1,39 @@
+package com.example.android.practiceset2udacity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    int scoreTeamA = 0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        displayForTeamA(scoreTeamA);
+    }
+
+    public void displayForTeamA (int score){
+        TextView scoreView = (TextView) findViewById(R.id.team_a_score);
+        scoreView.setText(String.valueOf(score));
+    }
+
+    public void addThreeForTeamA (View v){
+        scoreTeamA = scoreTeamA + 3;
+        displayForTeamA(scoreTeamA);
+    }
+
+    public void addTwoForTeamA (View v){
+        scoreTeamA = scoreTeamA + 2;
+        displayForTeamA( scoreTeamA);
+    }
+
+    public void addOneForTeamA (View v){
+        scoreTeamA = scoreTeamA + 1;
+        displayForTeamA( scoreTeamA );
+    }
+
+}
